90006d94a9edfa210d2069eafdd1fee44c489337
Fatpandac <i@fatpandac.com>
2025-05-24 18:00:10 +0800

    fix: add test for consts

--- lib/hackernews/src/consts.rs
+++ lib/hackernews/src/consts.rs
@@ -31,3 +31,23 @@ pub fn get_item_url(item_id: usize) -> String {
     format!("{}{}", API_BASE_URL, ITEM_ENDPOINT.replace("{}", &item_id.to_string()))
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_stories_url() {
+        assert_eq!(get_stories_url(StoryType::Top), format!("{}{}", API_BASE_URL, TOP_STORIES_ENDPOINT));
+        assert_eq!(get_stories_url(StoryType::New), format!("{}{}", API_BASE_URL, NEW_STORIES_ENDPOINT));
+        assert_eq!(get_stories_url(StoryType::Show), format!("{}{}", API_BASE_URL, SHOW_STORIES_ENDPOINT));
+        assert_eq!(get_stories_url(StoryType::Best), format!("{}{}", API_BASE_URL, BEST_STORIES_ENDPOINT));
+        assert_eq!(get_stories_url(StoryType::Jobs), format!("{}{}", API_BASE_URL, JOBS_ENDPOINT));
+    }
+
+    #[test]
+    fn test_get_item_url() {
+        let item_id = 12345;
+        assert_eq!(get_item_url(item_id), format!("{}item/{}.json", API_BASE_URL, item_id));
+    }
+}
+
